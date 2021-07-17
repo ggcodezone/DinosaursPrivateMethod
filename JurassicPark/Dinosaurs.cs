5257b4d29a72e8188ad3f998e36bd8ede92565fe
Dank <ggkalasky@gmail.com>
2021-07-17 16:09:08 -0400

    Test program that demonstrates simple use of private methods

---
+++ JurassicPark/Dinosaurs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JurassicPark
+{
+    class Dinosaurs
+    {
+        public string name;
+        public string species;
+        public int age;
+        public float softness; //stats about the animals 
+
+        public void PrintDetails() 
+        {
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Age:" + age);
+            Console.WriteLine("Species:" + species);
+            Console.WriteLine("Softness Lvl:" + softness);
+
+        } // will print info about our animals 
+
+        private void ReleaseTRex()
+        {
+            Console.WriteLine("The T-Rex was released into the petting zoo paddock.");
+        }
+        //Unless we need to use the paddock for temporary housing during renovation, we do not want T-Rex in petting zoo!
+        private void ReleaseRaptor()
+        {
+            Console.WriteLine("The Velociraptor was released into the petting zoo paddock.");
+        }
+        //Unless we need to use the paddock for temporary housing during renovation, we do not want Velociraptor in petting zoo!
+        public void ReleaseBaga()
+        {
+            Console.WriteLine("The Bagaceratops was released into the petting zoo paddock.");
+        }
+        //Bagaceratops is safe for interactions with guests and can be released without special authorization
+        public void ReleaseLiao()
+        {
+            Console.WriteLine("The Liaoceratops was released into the petting zoo paddock.");
+        }
+        //Liaoceratops is safe for interactions with guests and can be released without special authorization
+        public void ReleaseAgil()
+        {
+            Console.WriteLine("The Agilisaurus was released into the petting zoo paddock.");
+        }
+        //Agilisaurus is safe for interactions with guests and can be released without special authorization
+
+        public void PetDino()
+        {
+            Console.WriteLine("You petted" + name);
+            Console.WriteLine("His softness level is" + softness);
+        }
+        //animals can be petted with this method
+    }
+}
---
+++ JurassicPark/ZookeeperMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace JurassicPark
+{
+    class ZookeeperMain
+    {
+        static void Main(string[] args)
+        {
+            Dinosaurs MrBagido = new Dinosaurs();
+            MrBagido.name = "Mr. Bagido";
+            MrBagido.age = 3;
+            MrBagido.species = "T-Rex";
+            MrBagido.softness = 4;
+
+            Dinosaurs NiceBoi = new Dinosaurs();
+            NiceBoi.name = "Nice Boi";
+            NiceBoi.age = 2;
+            NiceBoi.species = "Bagaceratops";
+            NiceBoi.softness = 2;
+
+            Dinosaurs Smeegol = new Dinosaurs();
+            Smeegol.name = "Smeegol";
+            Smeegol.age = 4;
+            Smeegol.species = "Velociraptor";
+            Smeegol.softness = 5;
+
+            Dinosaurs Friend = new Dinosaurs();
+            Friend.name = "Friend";
+            Friend.age = 2;
+            Friend.species = "Agilisaurus";
+            Friend.softness = 3;
+
+            Friend.ReleaseBaga();
+
+            Friend.PrintDetails();
+
+        }
+    }
+}
